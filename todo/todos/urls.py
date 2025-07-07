7724e005530cdb9ecc8f092e262a6f8296652328
tboard20 <tboad04@gmail.com>
2025-07-07 19:52:06 +0100

    commit3

---
+++ todo/todos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('index/',views.index,name="index"),
+    # path('login/',views.login,name="login"),
+    # path('logout/',views.logout,name="logout"),
+    path('signup/',views.signup,name="signup"),
+]
--- todo/todos/views.py
+++ todo/todos/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 # Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+def index(request):
+    return render(request, "index.html")
+
+# def login(request):
+#     return render(request,"registration/login.html")
+
+# def logout(request):
+#     return render(request,"registration/logout.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form =UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('task_list')
+    else:
+            form =UserCreationForm()
+
+
+    return render(request,"registration/signup.html",{'form': form})
+    
