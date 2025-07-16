1bb16beb3f4f5fe912ae405301f692df88dec792
tboard20 <tboad04@gmail.com>
2025-07-16 11:14:21 +0100

    commit9

--- todos/models.py
+++ todos/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    deccription = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     Created_at =models.DateTimeField(auto_now_add=True)
 
