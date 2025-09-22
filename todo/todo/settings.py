f44109e8e3f0d1763a7bdff240b80d7cf9dec459
Olakunle Oluwatobiloba <tboad04@gmail.com>
2025-09-22 20:41:36 +0100

    commit14

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY", default="django-insecure-change-me")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 
 # Application definition
